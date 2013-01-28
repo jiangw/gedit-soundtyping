9dd6a6c7b76f37c5df1ffd03f56927201dda7333
jiangw <wj11hk@gmail.com>
2013-01-28 13:38:30 +0800

    different keys with different sound effects

--- playsoundtest.py
+++ playsoundtest.py
@@ -3,10 +3,19 @@ import pygame
 
 import time
 
-pygame.init()
+#pygame.init()
 
-pygame.mixer.music.load("typewriter-key-1.wav")
+#pygame.mixer.music.load("/home/jiang/Share/gedit/plugins/SoundTyping/123.wav")
 
-pygame.mixer.music.play()
+#pygame.mixer.music.play()
+
+#time.sleep(0.2)
+
+pygame.mixer.init()
+s1 = pygame.mixer.Sound("/home/jiang/Share/gedit/plugins/SoundTyping/ABC.wav")
+s2 = pygame.mixer.Sound("/home/jiang/Share/gedit/plugins/SoundTyping/abc.wav")
+
+s1.play()
+s2.play()
 
 time.sleep(0.2)
--- voicetyping.py
+++ voicetyping.py
@@ -7,16 +7,23 @@ class VoiceTypingWindowActivatable(GObject.Object, Gedit.WindowActivatable):
 
     window = GObject.property(type=Gedit.Window)
     doc = 0
+    snd_abc = 0
+    snd_ABC = 0
+    snd_123 = 0
+    snd_others = 0
 
     def __init__(self):
         GObject.Object.__init__(self)
-        pygame.init()
+        pygame.mixer.init()
 
     def do_activate(self):
         print "VoiceTypingPlugin: Start listening keyboard input."
         l_id = self.window.connect("key_press_event", self.do_key_pressed)
         self.window.VoiceTypingPluginHandlerId = l_id
-        pygame.mixer.music.load("/home/jiang/Share/gedit/plugins/typewriter-key-1.wav")
+        self.snd_abc = pygame.mixer.Sound("/home/jiang/Share/gedit/plugins/SoundTyping/abc.wav")
+        self.snd_ABC = pygame.mixer.Sound("/home/jiang/Share/gedit/plugins/SoundTyping/ABC.wav")
+        self.snd_123 = pygame.mixer.Sound("/home/jiang/Share/gedit/plugins/SoundTyping/123.wav")
+        self.snd_others = pygame.mixer.Sound("/home/jiang/Share/gedit/plugins/SoundTyping/others.wav")
 
     def do_deactivate(self):
         print "VoiceTypingPlugin: Stop listening keyboard input."
@@ -29,7 +36,17 @@ class VoiceTypingWindowActivatable(GObject.Object, Gedit.WindowActivatable):
         # changed, etc.)
         self.doc = self.window.get_active_document()
     
-    def do_key_pressed(self, event, data):
+    def do_key_pressed(self, window, event):
         if self.doc != None:
-            pygame.mixer.music.play()
-            time.sleep(0.001)
+            if event.keyval >= 0x030 and event.keyval <= 0x039:
+                self.snd_123.play()
+                time.sleep(0.001)
+            elif event.keyval >= 0x061 and event.keyval <= 0x07a:
+                self.snd_abc.play()
+                time.sleep(0.001)
+            elif event.keyval >= 0x041 and event.keyval <= 0x05a:
+                self.snd_ABC.play()
+                time.sleep(0.001)
+            else:
+                self.snd_others.play()
+                time.sleep(0.001)
